75e2fcf61a729e3f412235433bb7bd45b5094d44
Steven Shlimoon <44980375+shli3@users.noreply.github.com>
2020-03-26 18:53:17 -0400

    Edit Comments

--- BouncingBalls.py
+++ BouncingBalls.py
@@ -1,14 +1,13 @@
 def bouncingBall(h, bounce, window):
-    # your code
     counter = 0
     if h <= window or bounce <= 0 or bounce >= 1 or h < 0 or window < 0:
         return -1
     else:
         while h > window:
-            if h * bounce > window:
+            if h * bounce > window: #ball passes up and down
                 h = h * bounce
                 counter = counter + 2
-            else:
+            else: #ball just passes down
                 h = h * bounce
                 counter = counter + 1
     return counter
